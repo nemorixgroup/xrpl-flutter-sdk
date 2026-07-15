73ab3d414d4fbe36b94f04c6f738f4627bad60b0
Miguel Fagundez <59711011+miguelfagundez@users.noreply.github.com>
2026-07-15 13:07:36 -0400

    chore: export XrplBase58, shorten pubspec description and bump version to 0.0.2-dev

--- lib/xrpl_flutter_sdk.dart
+++ lib/xrpl_flutter_sdk.dart
@@ -4,6 +4,7 @@
 library;
 
 // Phase 1: Cryptographic fundamentals.
+export 'src/codec/xrpl_base58.dart';
 export 'src/crypto/xrpl_entropy.dart';
 export 'src/exceptions/xrpl_crypto_exception.dart';
 // export 'src/crypto/xrpl_key_algorithm.dart';
